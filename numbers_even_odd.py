98f7ff7a95a067b2db32f85421a1a432f3e4c7ee
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-04-26 05:39:55 +0800

    Added pseudocode

---
+++ numbers_even_odd.py
@@ -0,0 +1,5 @@
+# Introduction
+# Read integers from "number.txt" file
+# Separate even and odd
+# Write the even numbers to a separate text file (even.txt)
+# Write the odd numbers to a separate text file (odd.txt)
