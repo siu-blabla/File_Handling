8001c66cc335e8b113a2f5a3481b4558b7b733aa
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-04-26 06:12:26 +0800

    Separate the even and odd from the text file

--- numbers_even_odd.py
+++ numbers_even_odd.py
@@ -6,7 +6,10 @@ print('Instruction: This program creates new text files and categorizes '
 with open("numbers.txt", "r") as new_file:
     numbers = new_file.read().split()
     numbers = (int(num) for num in numbers)     # Assigning numbers to integer
+
 # Separate even and odd
+odd_numbers = (num for num in numbers if num % 2 == 1)
+even_numbers = (num for num in numbers if num % 2 != 0)
 
 # Write the even numbers to a separate text file (even.txt)
 # Write the odd numbers to a separate text file (odd.txt)
