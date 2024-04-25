4def28bd924c324729193b41e597c0768c02b19a
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-04-26 07:43:53 +0800

    Added pseudocode

---
+++ gwa_reader.py
@@ -0,0 +1,5 @@
+# Introduction
+# Assign the variable
+# Read the text file called, "student, data"
+# Identify the student with the highest GWA
+# Get the highest result of GWA and the name of the student behind it.
