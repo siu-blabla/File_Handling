6a9fbe0c82f69b19e155cf266991fa9a75a5f67c
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-04-26 05:59:19 +0800

    Added numbers.txt file

--- numbers_even_odd.py
+++ numbers_even_odd.py
@@ -3,6 +3,7 @@ print('Instruction: This program creates new text files and categorizes '
       'the numbers from "numbers.txt" into even and odd numbers.')
 
 # Read integers from "number.txt" file
+
 # Separate even and odd
 # Write the even numbers to a separate text file (even.txt)
 # Write the odd numbers to a separate text file (odd.txt)
