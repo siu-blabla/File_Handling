ba4cfc2156a5bea6f179abb5510c33209f371cff
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-04-26 06:21:21 +0800

    Write 'even.txt' file

--- numbers_even_odd.py
+++ numbers_even_odd.py
@@ -12,4 +12,7 @@ odd_numbers = (num for num in numbers if num % 2 == 1)
 even_numbers = (num for num in numbers if num % 2 != 0)
 
 # Write the even numbers to a separate text file (even.txt)
+with open("even.txt", "w") as even_integers_file:
+    for num in even_numbers:
+        even_integers_file.write(str(num) + "\n")
 # Write the odd numbers to a separate text file (odd.txt)
