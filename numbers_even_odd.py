a750faf44db2131daa14353c19917b10476168eb
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-04-26 06:43:27 +0800

    Write 'odd.txt' file

--- numbers_even_odd.py
+++ numbers_even_odd.py
@@ -8,11 +8,15 @@ with open("numbers.txt", "r") as new_file:
     numbers = (int(num) for num in numbers)     # Assigning numbers to integer
 
 # Separate even and odd
-odd_numbers = (num for num in numbers if num % 2 == 1)
-even_numbers = (num for num in numbers if num % 2 != 0)
+even_numbers = [num for num in numbers if num % 2 == 0]
+odd_numbers = [num for num in numbers if num % 2 != 0]
 
 # Write the even numbers to a separate text file (even.txt)
-with open("even.txt", "w") as even_integers_file:
+with open("even.txt", "w") as even_file:
     for num in even_numbers:
-        even_integers_file.write(str(num) + "\n")
+        even_file.write(str(num) + "\n")
+
 # Write the odd numbers to a separate text file (odd.txt)
+with open("odd.txt", "w") as odd_file:
+    for num in odd_numbers:
+        odd_file.write(str(num) + "\n")
