336aaae432f08d6a85f21e8aa082958102cd28d7
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-04-26 06:08:19 +0800

    Made the program read the text file and converted every number to integer

--- numbers_even_odd.py
+++ numbers_even_odd.py
@@ -3,7 +3,10 @@ print('Instruction: This program creates new text files and categorizes '
       'the numbers from "numbers.txt" into even and odd numbers.')
 
 # Read integers from "number.txt" file
-
+with open("numbers.txt", "r") as new_file:
+    numbers = new_file.read().split()
+    numbers = (int(num) for num in numbers)     # Assigning numbers to integer
 # Separate even and odd
+
 # Write the even numbers to a separate text file (even.txt)
 # Write the odd numbers to a separate text file (odd.txt)
