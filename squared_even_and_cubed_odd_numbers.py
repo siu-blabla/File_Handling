f479662c8a0bbed827c79df59bdae21b8233bd5e
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-04-26 16:03:01 +0800

    Added pseudocode

---
+++ squared_even_and_cubed_odd_numbers.py
@@ -0,0 +1,8 @@
+# Introduction
+# Read numbers from integers.txt file
+# Separate the data/ get the even and odd numbers
+# Calculation: Square all the even numbers
+# Calculations: Cube all the odd numbers
+# Creating squared even text file
+# Creating cubed odd text file
+# Successful Execution Message
