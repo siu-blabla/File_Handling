0f82960bf6ddc45dd6481fe16b7b63a9b8754901
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-04-26 12:35:08 +0800

    Let the program read the text file called, 'student_data.txt'

--- gwa_reader.py
+++ gwa_reader.py
@@ -5,5 +5,7 @@ print('Instruction: This program reveals the student with the highest'
 highest_general_weighted_average = float('inf')
 name_of_student = ''
 # Read the text file called, "student_data.txt"
+with open("student_data.txt", "r") as file:
+    datas = file.readlines()
 # Identify the student with the highest GWA
 # Get the highest result of GWA and the name of the student behind it.
