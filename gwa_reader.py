fa38b51cde1a6f3054de552e4daf5c711659b480
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-04-26 12:29:06 +0800

    Assigned the variables

--- gwa_reader.py
+++ gwa_reader.py
@@ -2,6 +2,8 @@
 print('Instruction: This program reveals the student with the highest'
       'Grade Weighted Average (GWA) and their corresponding GWA.')
 # Assign the variable
-# Read the text file called, "student, data"
+highest_general_weighted_average = float('inf')
+name_of_student = ''
+# Read the text file called, "student_data.txt"
 # Identify the student with the highest GWA
 # Get the highest result of GWA and the name of the student behind it.
