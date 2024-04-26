2502e6b5454802e16b91a2c102ac18cf7aace554
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-04-26 16:12:36 +0800

    Make the program read the text file

--- squared_even_and_cubed_odd_numbers.py
+++ squared_even_and_cubed_odd_numbers.py
@@ -2,6 +2,8 @@
 print("Instruction: This program squares even numbers and cubes odd numbers from 'integer.txt'"
       "creating a new text file with the results.")
 # Read numbers from integers.txt file
+with open("integers.txt", "r") as new_file:
+    integers = [int(line.strip()) for line in new_file]
 # Separate the data/ get the even and odd numbers
 # Calculation: Square all the even numbers
 # Calculations: Cube all the odd numbers
