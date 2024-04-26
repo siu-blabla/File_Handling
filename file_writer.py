5daeb586f34a212dda21fd24819f3ab1dce6c65a
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-04-26 14:27:44 +0800

    Added looping

--- file_writer.py
+++ file_writer.py
@@ -4,3 +4,10 @@ print("This program writes multiple line of text into a text file 'mylife.txt'")
 with open("mylife.txt", "a") as file:
 
     # Looping
+    while True:
+        enter_line = input("Enter Line: ")
+        file.write(enter_line + "\n")
+        more_lines = input("Are there more lines y/n? ")
+        if more_lines != 'y':
+            break
+        print("Thank you for using this program. Come again sometime!")
