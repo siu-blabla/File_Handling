d1e610795bf036438cd687506a49e88977c251f7
siu-blabla <107726897+siu-blabla@users.noreply.github.com>
2024-04-26 16:08:36 +0800

    Added instruction for the users of the program

--- squared_even_and_cubed_odd_numbers.py
+++ squared_even_and_cubed_odd_numbers.py
@@ -1,4 +1,6 @@
 # Introduction
+print("Instruction: This program squares even numbers and cubes odd numbers from 'integer.txt'"
+      "creating a new text file with the results.")
 # Read numbers from integers.txt file
 # Separate the data/ get the even and odd numbers
 # Calculation: Square all the even numbers
